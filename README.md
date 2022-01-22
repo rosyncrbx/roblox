b71fc5ac9e71e6c54e2b7feea3e977c02d9ded2c
xynnylol <xynnylol@gmail.com>
2022-01-22 00:23:31 +0000

    Remove test files.

--- README.md
+++
@@ -1,4 +0,0 @@
-# Roblox
-All Roblox stuff. ClassNames and properties. Enums. Latest version. Etc. Auto updates.
-
-If you want a specific version of Roblox go through the changes and look for the version you want.
--- Update.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-git add "Client.zip"
-git add "ClientVersion.txt"
-git commit -m "Updated to %1"
-git add "DeployHistory.txt"
-git commit -m "Deploy history changed"
-git add -A
-git commit -m "Updated to %2'
-git push
